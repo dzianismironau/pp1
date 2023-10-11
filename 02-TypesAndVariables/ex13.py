ea0aa4a2479fae0ddef8ae07496ad7410a2a92eb
s-013-17 <s-013-17@gumiforest.ae.krakow.pl>
2023-10-11 13:00:52 +0200

    My first Python programs from textbook

---
+++ 02-TypesAndVariables/ex13.py
@@ -0,0 +1,3 @@
+cubeSide = input('Enter cube side: ')
+surfArea = int(cubeSide)**3
+print(f'The surface area of a cube with side {cubeSide} is {surfArea}')
---
+++ 02-TypesAndVariables/ex8.py
@@ -0,0 +1,6 @@
+int1 = 71
+int2 = 14
+result = int1 + int2
+print('Number1: ', int1)
+print('Number2: ', int2)
+print('The result of summation: ', result)
